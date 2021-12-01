7d39dd633afe0a823cab5b2311182a656526fc4d
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2021-12-01 12:08:27 +0100

    norm

--- 2021/01/main.ts
+++ 2021/01/main.ts
@@ -1,17 +1,18 @@
+// Setup.
 const inputs = (await Deno.readTextFile("./data/final.txt"))
     .split("\n")
     .map(Number);
 
+// Part One.
 const single = inputs.reduce((increases, value, index) => {
     return increases + (index > 0 && value > inputs[index - 1] ? 1 : 0);
 }, 0);
-
 console.log('Answer Part One: ', single);
 
+// Part Two.
 const sliding =  inputs.reduce((increases, _, index) => {
     const a = inputs[index] + inputs[index + 1] + inputs[index + 2];
     const b = inputs[index+1] + inputs[index + 2] + inputs[index + 3];
     return increases + (index < inputs.length - 3  && b > a ? 1 : 0);
 }, 0);
-
 console.log('Answer Part Two: ', sliding);
