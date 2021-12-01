f72701e347a8b9cc4495aef962b0d9ba7be13046
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2021-12-01 11:35:28 +0100

    2021-01

---
+++ 2021/01/main.ts
@@ -0,0 +1,17 @@
+const inputs = (await Deno.readTextFile("./data/final.txt"))
+    .split("\n")
+    .map(Number);
+
+const single = inputs.reduce((increases, value, index) => {
+    return increases + (index > 0 && value > inputs[index - 1] ? 1 : 0);
+}, 0);
+
+console.log('Answer Part One: ', single);
+
+const sliding =  inputs.reduce((increases, _, index) => {
+    const a = inputs[index] + inputs[index + 1] + inputs[index + 2];
+    const b = inputs[index+1] + inputs[index + 2] + inputs[index + 3];
+    return increases + (index < inputs.length - 3  && b > a ? 1 : 0);
+}, 0);
+
+console.log('Answer Part Two: ', sliding);
---
+++ 2021/01/serve.sh
@@ -0,0 +1,1 @@
+deno run --watch --allow-read main.ts
